9d4eefe4b6405713e1390d1a7c8b4d961f6571bf
Estefania Rodriguez <estefania.rodriguez.1313@hotmail.com>
2022-05-23 01:18:48 -0300

    API NET CORE

---
+++ APINETCORE/Data/APINETCOREContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APINETCORE.Modelo;
+
+namespace APINETCORE.Data
+{
+    public class APINETCOREContext : DbContext
+    {
+        public APINETCOREContext (DbContextOptions<APINETCOREContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APINETCORE.Modelo.Producto>? Producto { get; set; }
+    }
+}
---
+++ APINETCORE/Modelo/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace APINETCORE.Modelo
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string? Nombre_Producto { get; set; }
+
+        public double Precio_Venta { get; set; }
+
+        public double Precio_Compra { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
